39b49c768b6291702532d60885848b7aac27d7b0
Kevin Mahoney <kjmahoney13@gmail.com>
2017-08-03 03:00:11 -0400

    create promise to face direction after movement

--- js/game.js
+++ js/game.js
@@ -6,17 +6,24 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 
 function preload() {
   this.load.spritesheet('knight', 'assets/knight.png', 64, 64, 200);
+  this.load.spritesheet('archer', 'assets/archer.png', 64, 64, 200);
 }
 
 function create() {
   this.player = this.add.sprite(this.game.width / 2, this.game.height - 300, 'knight');
   this.player.anchor.setTo(0.5, 0.5);
-  this.player.animations.add('spear', [58 ], 20, true);
+  this.player.animations.add('spear-up', [9], 20, true);
+  this.player.animations.add('spear-left', [58], 20, true);
+  this.player.animations.add('spear-down', [107], 20, true);
+  this.player.animations.add('spear-right', [156], 20, true);
+  this.player.animations.add('bow-front', [0], 20, true);
+  this.player.play('spear-front');
 
   this.physics.enable(this.player, Phaser.Physics.ARCADE);
-  this.player.speed = 100;
+  this.player.speed = 200;
   setMovementAnimations(this.player);
   this.cursors = this.input.keyboard.createCursorKeys();
+  console.log(this.cursors);
 }
 
 function update() {
--- js/player-movement.js
+++ js/player-movement.js
@@ -1,35 +1,67 @@
 const setMovementAnimations = function(player) {
-  player.animations.add('walk-north', [2,3], 20, true);
-  player.animations.add('walk-south', [100,101], 20, true);
-  player.animations.add('walk-west', [51,52], 20, true);
-  player.animations.add('walk-east', [149,150], 20, true);
+  player.animations.add('walk-up', [2,3], 20, true);
+  player.animations.add('walk-down', [100,101], 20, true);
+  player.animations.add('walk-left', [51,52], 20, true);
+  player.animations.add('walk-right', [149,150], 20, true);
 }
+
+const moveUp = function(player, cursors){
+    player.body.velocity.y = -player.speed
+    player.play(`walk-up`, 5)
+}
+
+const moveDown = function(player){
+    player.body.velocity.y = player.speed
+    player.play('walk-down', 5);
+}
+
+const moveRight = function(player){
+    player.body.velocity.x = player.speed;
+    player.play('walk-right', 5);
+}
+
+const moveLeft = function(player){
+    player.body.velocity.x = - player.speed;
+    player.play('walk-left', 5)
+}
+//Should probably move to a preset page
+let direction = 'up'
 
 const playerMovement = function(cursors, player) {
-  player.body.velocity.y = 0;
-  player.body.velocity.x = 0;
+  return new Promise((resolve, reject) => {
 
-  if (cursors.up.isUp &&
-      cursors.down.isUp &&
-      cursors.right.isUp &&
-      cursors.left.isUp
-    ) {
-    player.play('spear')
-  }
+    player.body.velocity.y = 0;
+    player.body.velocity.x = 0;
 
-  if (cursors.up.isDown) {
-    player.body.velocity.y = -player.speed;
-    player.play('walk-north', 5);
-  } else if (cursors.down.isDown) {
-    player.body.velocity.y = player.speed;
-    player.play('walk-south', 5)
-  }
+    if (cursors.up.isDown) {
+      moveUp(player)
+      direction = 'up'
+    }
 
-  if (cursors.left.isDown) {
-    player.body.velocity.x = - player.speed;
-    player.play('walk-west', 5)
-  } else if (cursors.right.isDown) {
-    player.body.velocity.x = player.speed;
-    player.play('walk-east', 5);
-  }
+    if (cursors.down.isDown) {
+      moveDown(player)
+      direction = 'down'
+    }
+
+    if (cursors.left.isDown) {
+      moveLeft(player)
+      direction = 'left'
+    }
+
+    if (cursors.right.isDown) {
+      moveRight(player)
+      direction = 'right'
+    }
+
+    if (cursors.up.isUp &&
+        cursors.down.isUp &&
+        cursors.right.isUp &&
+        cursors.left.isUp) {
+          resolve(direction, player);
+        }
+  }).then((direction) => {
+    player.play(`spear-${direction}`)
+  })
 }
+//weapons function, name weapon and direction and gives where he faces
+//change all forwards ups etc to one convention
